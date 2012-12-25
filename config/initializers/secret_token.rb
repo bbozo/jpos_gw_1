2326fedc25bff1441289a5f76c7104535460b64e
Borna Novak <dosadnizub@gmail.com>
2012-12-25 05:42:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JposGw1::Application.config.secret_token = '96c2da816944be83d1589d6928a54441b324ef960072ccd14125ae01364dd4b99bd35b7d58214271488e7b2a7d2fc2ccaad6c9548c2592749927f165c37683e8'
